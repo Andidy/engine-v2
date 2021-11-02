94379515677767a0e26a198d29ab43b56492cf0b
Andidy <37605997+Andidy@users.noreply.github.com>
2021-11-01 19:49:23 -0500

    Added `cRenderable` index to `Entity`

--- engine-v2/src/entity.h
+++ engine-v2/src/entity.h
@@ -9,23 +9,23 @@ struct cTransform {
 };
 
 struct Entity {
-	std::string name;
+	std::string name = "Unnamed";
 	int64_t id = -1;
 	bool is_active = false;
 
 	int32_t transform = -1;
 	int32_t grid_transform = -1;
+	int32_t renderable = -1;
 	int32_t citem = 1;
 
-	Entity() {
-		name = "unnamed";
-	}
+	Entity() = default;
 
-	Entity(std::string _name, int64_t _id, int32_t _transform, int32_t _grid_transform) {
+	Entity(std::string _name, int64_t _id, int32_t _transform, int32_t _grid_transform, int32_t _renderable) {
 		name = _name;
 		id = _id;
 		transform = _transform;
 		grid_transform = _grid_transform;
+		renderable = _renderable;
 	}
 };
 
