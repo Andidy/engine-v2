c36d151d14046a667ac08bd458d0fde8e0871de5
Andidy <37605997+Andidy@users.noreply.github.com>
2021-11-13 18:19:56 -0600

    Implemented Initial Action System
    The initial action system has been implemented. Actions are registered to an input method through `GameState::RegisterAction()` and will live in `GameState::action_map`. Then during the beginning of our main loop we check if any buttons were pressed or released and if they correspond to an `Action` that has been registered. We enqueue actions that occur into `GameState::action_queue`. Then in the game loop we pull the actions out of the queue and set the action boolean to true that is stored in `GameState` (this is an area that needs work). We then use the action booleans to execute the functionality that should occur this game loop.
    
    Areas that could use improvement:
    - Joystick actions, issue #51
    - MouseWheel action
    - Moving the input method adjustment functions out of `action.h/.c` and into a more general purpose raylib helper

--- engine-v2/src/action.cpp
+++ engine-v2/src/action.cpp
@@ -1,2 +1,46 @@
 #include "action.h"
 
+// Action System
+
+Action::Action(const std::string& _action_name, ActionType _type) {
+	action = _action_name;
+	type = _type;
+}
+
+void Action::PrintString() {
+	std::cout << "Action: " << action << ", " << (int)type << "\n";
+}
+
+// Functions to shift raylib key codes into safe regions
+
+int rl_KeyboardToCode(int keycode) {
+	return keycode;
+}
+
+int rl_MouseToCode(int keycode) {
+	return MOUSE_CODE_OFFSET + keycode;
+}
+
+int rl_ControllerToCode(int keycode) {
+	return CONTROLLER_CODE_OFFSET + keycode;
+}
+
+int rl_ControllerAxisToCode(int axis) {
+	return CONTROLLER_CODE_OFFSET + MAX_GAMEPAD_BUTTONS + axis;
+}
+
+int rl_CodeToKeyboard(int code) {
+	return code;
+}
+
+int rl_CodeToMouse(int code) {
+	return code - MOUSE_CODE_OFFSET;
+}
+
+int rl_CodeToController(int code) {
+	return code - CONTROLLER_CODE_OFFSET;
+}
+
+int rl_CodeToControllerAxis(int code) {
+	return code - CONTROLLER_CODE_OFFSET - MAX_GAMEPAD_BUTTONS;
+}
--- engine-v2/src/action.h
+++ engine-v2/src/action.h
@@ -2,6 +2,8 @@
 
 #include "universal.h"
 
+// Action System
+
 enum class ActionType {
 	START,
 	END
@@ -9,6 +11,22 @@ enum class ActionType {
 
 struct Action {
 	std::string action;
-	// int action_hash;
+	// int action_hash; // this will replace the string version of action to lower the amount of memory being passed around
+	ActionType type;
 
+	Action(const std::string& action_name, ActionType type);
+	void PrintString();
 };
+
+// Key code adjustment constants and functions
+const int MOUSE_CODE_OFFSET = 400;
+const int CONTROLLER_CODE_OFFSET = 500;
+
+int rl_KeyboardToCode(int keycode);
+int rl_MouseToCode(int keycode);
+int rl_ControllerToCode(int keycode);
+int rl_ControllerAxisToCode(int axis);
+int rl_CodeToKeyboard(int code);
+int rl_CodeToMouse(int code);
+int rl_CodeToController(int code);
+int rl_CodeToControllerAxis(int code);
