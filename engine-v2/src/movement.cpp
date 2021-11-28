4cc7bc33f0722678e730c82651bf48478147fd1b
Andidy <37605997+Andidy@users.noreply.github.com>
2021-11-27 22:08:46 -0600

    Improved `FloodFill()` readability by reducing redundant code for checking neighbors. `FloodFill()` also uses terrain costs now. Fixed graphical bug with attack range shading duplicating on some tiles. Fixed graphical bug with attack range shading displaying over movement range shading. Implemented AStar search for pathfinding.

--- engine-v2/src/movement.cpp
+++ engine-v2/src/movement.cpp
@@ -55,7 +55,7 @@ BuildPathContext BuildPath(Vector2 start, Vector2 end, int movement_points) {
 
 void FloodFill(FloodFillContext& ffc) {
 	ffc.frontier.push(ffc.start);
-	ffc.cost[ffc.start] = 0.0f;
+	ffc.cost[ffc.start] = 0;
 
 	while (!ffc.frontier.empty()) {
 		auto curr_tile = ffc.frontier.front();
@@ -63,151 +63,149 @@ void FloodFill(FloodFillContext& ffc) {
 
 		if (ffc.visited.find(curr_tile) == ffc.visited.end()) {
 			ffc.visited.insert(curr_tile);
-			ffc.visited_vec.push_back(curr_tile);
 			auto cost = ffc.cost[curr_tile];
+
+			const int num_directions = 4;
+			IVector2 directions[num_directions] = {
+				{curr_tile.x, curr_tile.y - 1}, // north
+				{curr_tile.x, curr_tile.y + 1}, // south
+				{curr_tile.x - 1, curr_tile.y}, // west
+				{curr_tile.x + 1, curr_tile.y}  // east
+			};
 
 			if (cost < ffc.remaining_movement_points) {
 				ffc.finalCost[curr_tile] = cost;
 				
-				// Check North
-				IVector2 north = { curr_tile.x, curr_tile.y - 1 };
-				if (ffc.visited.find(north) == ffc.visited.end()) {
-					int pathCost = cost + 1;
-					
-					int cost_so_far = INT_MAX;
-					if (ffc.cost.find(north) != ffc.cost.end()) {
-						cost_so_far = ffc.cost[north];
+				for (int i = 0; i < num_directions; i++) {
+					IVector2 dir = directions[i];
+					if (ffc.gm->tiles.find(dir) != ffc.gm->tiles.end()) {
+						if (ffc.gm->tiles[dir].is_land) {
+							if (ffc.visited.find(dir) == ffc.visited.end()) {
+								int pathCost = cost + terrain_costs[ffc.gm->tiles[dir].terrain_type];
+
+								int cost_so_far = INT_MAX;
+								if (ffc.cost.find(dir) != ffc.cost.end()) {
+									cost_so_far = ffc.cost[dir];
+								}
+
+								if (pathCost < cost_so_far) {
+									ffc.cost[dir] = pathCost;
+								}
+
+								ffc.frontier.push(dir);
+							}
+						}
 					}
-
-					if (pathCost < cost_so_far) {
-						ffc.cost[north] = pathCost;
-					}
-
-					ffc.frontier.push(north);
-				}
-
-				// Check South
-				IVector2 south = { curr_tile.x, curr_tile.y + 1 };
-				if (ffc.visited.find(south) == ffc.visited.end()) {
-					int pathCost = cost + 1;
-
-					int cost_so_far = INT_MAX;
-					if (ffc.cost.find(south) != ffc.cost.end()) {
-						cost_so_far = ffc.cost[south];
-					}
-
-					if (pathCost < cost_so_far) {
-						ffc.cost[south] = pathCost;
-					}
-
-					ffc.frontier.push(south);
-				}
-
-				// Check West
-				IVector2 west = { curr_tile.x - 1, curr_tile.y };
-				if (ffc.visited.find(west) == ffc.visited.end()) {
-					int pathCost = cost + 1;
-
-					int cost_so_far = INT_MAX;
-					if (ffc.cost.find(west) != ffc.cost.end()) {
-						cost_so_far = ffc.cost[west];
-					}
-
-					if (pathCost < cost_so_far) {
-						ffc.cost[west] = pathCost;
-					}
-
-					ffc.frontier.push(west);
-				}
-
-				// Check East
-				IVector2 east = { curr_tile.x + 1, curr_tile.y };
-				if (ffc.visited.find(east) == ffc.visited.end()) {
-					int pathCost = cost + 1;
-
-					int cost_so_far = INT_MAX;
-					if (ffc.cost.find(east) != ffc.cost.end()) {
-						cost_so_far = ffc.cost[east];
-					}
-
-					if (pathCost < cost_so_far) {
-						ffc.cost[east] = pathCost;
-					}
-
-					ffc.frontier.push(east);
 				}
 			}
 			else { // store final boundary?
-				ffc.finalCost[curr_tile] = cost;
+				for (int i = 0; i < num_directions; i++) {
+					IVector2 dir = directions[i];
+					if (ffc.gm->tiles.find(dir) != ffc.gm->tiles.end()) {
+						if (ffc.gm->tiles[dir].is_land) {
+							if (ffc.visited.find(dir) == ffc.visited.end()) {
+								int pathCost = cost + 1;
 
-				// Check North
-				IVector2 north = { curr_tile.x, curr_tile.y - 1 };
-				if (ffc.visited.find(north) == ffc.visited.end()) {
-					int pathCost = cost + 1;
+								int cost_so_far = INT_MAX;
+								if (ffc.cost.find(dir) != ffc.cost.end()) {
+									cost_so_far = ffc.cost[dir];
+								}
 
-					int cost_so_far = INT_MAX;
-					if (ffc.cost.find(north) != ffc.cost.end()) {
-						cost_so_far = ffc.cost[north];
+								if (pathCost < cost_so_far) {
+									ffc.cost[dir] = pathCost;
+								}
+
+								ffc.edge_frontier.insert(dir);
+							}
+						}
 					}
-
-					if (pathCost < cost_so_far) {
-						ffc.cost[north] = pathCost;
-					}
-
-					ffc.edge_frontier.push_back(north);
-				}
-
-				// Check South
-				IVector2 south = { curr_tile.x, curr_tile.y + 1 };
-				if (ffc.visited.find(south) == ffc.visited.end()) {
-					int pathCost = cost + 1;
-
-					int cost_so_far = INT_MAX;
-					if (ffc.cost.find(south) != ffc.cost.end()) {
-						cost_so_far = ffc.cost[south];
-					}
-
-					if (pathCost < cost_so_far) {
-						ffc.cost[south] = pathCost;
-					}
-
-					ffc.edge_frontier.push_back(south);
-				}
-
-				// Check West
-				IVector2 west = { curr_tile.x - 1, curr_tile.y };
-				if (ffc.visited.find(west) == ffc.visited.end()) {
-					int pathCost = cost + 1;
-
-					int cost_so_far = INT_MAX;
-					if (ffc.cost.find(west) != ffc.cost.end()) {
-						cost_so_far = ffc.cost[west];
-					}
-
-					if (pathCost < cost_so_far) {
-						ffc.cost[west] = pathCost;
-					}
-
-					ffc.edge_frontier.push_back(west);
-				}
-
-				// Check East
-				IVector2 east = { curr_tile.x + 1, curr_tile.y };
-				if (ffc.visited.find(east) == ffc.visited.end()) {
-					int pathCost = cost + 1;
-
-					int cost_so_far = INT_MAX;
-					if (ffc.cost.find(east) != ffc.cost.end()) {
-						cost_so_far = ffc.cost[east];
-					}
-
-					if (pathCost < cost_so_far) {
-						ffc.cost[east] = pathCost;
-					}
-
-					ffc.edge_frontier.push_back(east);
 				}
 			}
+		}
+	}
+
+	for (auto& node : ffc.visited) {
+		ffc.edge_frontier.extract(node);
+	}
+}
+
+int AStarHeuristic(IVector2 start, IVector2 end) {
+	return abs(start.x - end.x) + abs(start.y - end.y);
+}
+
+const int UP = 1;
+const int DOWN = 2;
+const int LEFT = 3;
+const int RIGHT = 4;
+
+void AStar(AStarContext& asc) {
+	asc.frontier.put(asc.start, 0);
+	asc.from[asc.start] = asc.start;
+	asc.costs[asc.start] = 0;
+
+	while (!asc.frontier.empty()) {
+		IVector2 curr_tile = asc.frontier.get();
+
+		if (curr_tile == asc.goal) {
+			asc.found_path = true;
+			break;
+		}
+
+		if (asc.costs[curr_tile] >= asc.remaining_movement_points) {
+			continue;
+		}
+
+		const int num_directions = 4;
+		IVector2 directions[num_directions] = {
+			{curr_tile.x, curr_tile.y - 1}, // north
+			{curr_tile.x, curr_tile.y + 1}, // south
+			{curr_tile.x - 1, curr_tile.y}, // west
+			{curr_tile.x + 1, curr_tile.y}  // east
+		};
+
+		for (int i = 0; i < num_directions; i++) {
+			Tile t = asc.gm->tiles[directions[i]];
+			if (t.is_land) {
+				int new_cost = asc.costs[curr_tile] + terrain_costs[t.terrain_type];
+
+				if (asc.costs.find(directions[i]) == asc.costs.end() || new_cost < asc.costs[directions[i]]) {
+					asc.costs[directions[i]] = new_cost;
+					int priority = new_cost + AStarHeuristic(directions[i], asc.goal);
+					asc.frontier.put(directions[i], priority);
+					asc.from[directions[i]] = curr_tile;
+				}
+			}
+		}
+	}
+
+	if (!asc.found_path) {
+		return;
+	}
+	else {
+		IVector2 ghost = asc.goal;
+		
+		while (ghost != asc.start) {
+			IVector2 prev = asc.from[ghost];
+			IVector2 delta = ghost - prev;
+
+			if (delta.x == 1) {
+				// RIGHT
+				asc.path.push_front(RIGHT);
+			}
+			else if (delta.x == -1) {
+				// LEFT
+				asc.path.push_front(LEFT);
+			}
+			else if (delta.y == 1) {
+				// DOWN
+				asc.path.push_front(DOWN);
+			}
+			else if (delta.y == -1) {
+				// UP
+				asc.path.push_front(UP);
+			}
+
+			ghost = prev;
 		}
 	}
 }
--- engine-v2/src/movement.h
+++ engine-v2/src/movement.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "universal.h"
+#include "grid_map.h"
 
 int ManhattanDistance(Vector2 start, Vector2 end);
 
@@ -16,11 +17,49 @@ struct FloodFillContext {
 	IVector2 start = {0, 0};
 	int remaining_movement_points = 0;
 	std::unordered_set<IVector2, IVector2Hash> visited;
-	std::vector<IVector2> visited_vec;
-	std::vector<IVector2> edge_frontier;
+	std::unordered_set<IVector2, IVector2Hash> edge_frontier;
 	std::unordered_map<IVector2, int, IVector2Hash> cost;
 	std::unordered_map<IVector2, int, IVector2Hash> finalCost;
 	std::queue<IVector2> frontier;
+
+	GridMap* gm;
 };
 
 void FloodFill(FloodFillContext& ffc);
+
+template<typename T, typename priority_t>
+struct PriorityQueue {
+	typedef std::pair<priority_t, T> PQElement;
+	
+	struct Compare {
+		bool operator() (PQElement lhs, PQElement rhs) {
+			return lhs.first > rhs.first;
+		}
+	};
+	
+	std::priority_queue<PQElement, std::vector<PQElement>, Compare> elements;
+
+	inline bool empty() const { return elements.empty(); }
+	inline void put(T item, priority_t priority) { elements.emplace(priority, item); }
+	T get() {
+		T top = elements.top().second;
+		elements.pop();
+		return top;
+	}
+};
+
+struct AStarContext {
+	bool found_path = false;
+	int remaining_movement_points = 0;
+
+	IVector2 start = { 0, 0 };
+	IVector2 goal = { 0, 0 };
+	PriorityQueue<IVector2, int> frontier;
+	std::unordered_map<IVector2, IVector2, IVector2Hash> from;
+	std::unordered_map<IVector2, int, IVector2Hash> costs;
+	std::deque<int> path;
+
+	GridMap* gm;
+};
+
+void AStar(AStarContext& asc);
